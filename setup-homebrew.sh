f5eab0189987a8a405b132c179799dc6f20c7596
Hodaka Suzuki <altitude3190@gmail.com>
2019-05-30 23:42:08 +0900

    path to diff-highlight

--- setup-homebrew.sh
+++ setup-homebrew.sh
@@ -10,3 +10,7 @@ fi
 # 'Error: Too many levels of symbolic links @ rb_sysopen - ~/.Brewfile'と怒られるので、
 # --fileで.Brewfileを直指定する
 brew bundle --file=.Brewfile
+
+# gitのdiffを見やすくするために、diff-highlightにパスを通す
+# cf. https://qiita.com/takyam/items/d6afacc7934de9b0e85e
+ln -s /usr/local/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
